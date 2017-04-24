d12e4563fab3628715d4ad32f16339904b032e43
marcorodnav <marcorodnav@gmail.com>
2017-04-24 15:55:09 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+var morseAlphabet = [
+  {letter: "a", val: '.-'},
+  {letter: "b", val: '-...'},
+  {letter: "c", val: '-.-.'},
+  {letter: "d", val: '-..'},
+  {letter: "e", val: '.'},
+  {letter: "f", val: '..-.'},
+  {letter: "g", val: '--.'},
+  {letter: "h", val: '....'},
+  {letter: "i", val: '..'},
+  {letter: "j", val: '.---'},
+  {letter: "k", val: '-.-'},
+  {letter: "l", val: '.-..'},
+  {letter: "m", val: '--'},
+  {letter: "n", val: '-.'},
+  {letter: "o", val: '---'},
+  {letter: "p", val: '.--.'},
+  {letter: "q", val: '--.-'},
+  {letter: "r", val: '.-.'},
+  {letter: "s", val: '...'},
+  {letter: "t", val: '-'},
+  {letter: "u", val: '..-'},
+  {letter: "v", val: '...-'},
+  {letter: "w", val: '.--'},
+  {letter: "x", val: '-..-'},
+  {letter: "y", val: '-.--'},
+  {letter: "z", val: '--..'}
+];
+$().ready(function(){
+    console.warn("Eureka!!");
+});
+
+$('#calculate').on('click', function() {
+  console.log("Begins calculation");
+});
